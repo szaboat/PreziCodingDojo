a68423f87da985f7860b2217b606f8cb327cab51
Attila Szabo <attila.szabo@prezi.com>
2012-10-26 09:55:35 +0200

    result from of the dojo

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,35 @@
 var calculator = {
-    add: function(){
-        return 0;
-    }
+   add: function(expression){
+       var delimiters = [',', '\n'];
+       if (expression === '') {
+           return 0;
+       }
+       var delimiter_regexp = /^\/\/(.+)\n(.+)$/g
+       var mat = delimiter_regexp.exec(expression);
+       if (mat && mat.length) {
+           var delimiters = mat[1].substr(1).substr(0, mat[1].length - 2).split("][");
+           expression = mat[2];
+       }
+       var delimiter = '(' + delimiters.join(")|(") + ')';
+       delimiter = delimiter.replace(/\*/g, '\\*');
+       console.log(delimiter);
+       var numbers = expression.split(new RegExp(delimiter));
+       var result = 0;
+       var errorString = "";
+       for (var i=0; i < numbers.length; i++)
+       {
+           var number = parseInt(numbers[i]);
+           if (number < 0)
+           {
+               errorString += (errorString.length > 0 ? ", " : " ") + number;
+           } else if (number <= 1000 ) {
+               result += number;
+           }
+       }
+       if (errorString.length > 0)
+       {
+           throw "negatives not allowed:" + errorString;
+       }
+       return result;
+   }
 }
--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -1,10 +1,44 @@
 describe("Calculator", function() {
-    describe("Basic", function() {
-        it("should evaluate '' to 0", function() {
-            expect(calculator.add('')).toBe(0);
-        });
-        it("should evaluate '42' to 42", function() {
-            expect(calculator.add('42')).toBe(42);
-        });
+    describe("Basics", function() {
+        calculator_helper('', 0);
+        calculator_helper('42', 42);
+        calculator_helper('42,22', 64);
+        calculator_helper('1,2,3,4', 10);
+        calculator_helper('1,2,3,4,10,10', 30);
+        calculator_helper('1,2,3,4,10\n10', 30);
+        calculator_helper('1,2,3,4\n10\n10', 30);
+        calculator_helper('//;\n1;2', 3);
+    });
+    describe("Chapter 'e'", function(){
+        it("should throw error for: '-5'", function (){
+            expect(function () {calculator.add('-5');}).toThrow("negatives not allowed: -5");
+        })
+        it("should throw error for: '-15'", function (){
+            expect(function () {calculator.add('-15');}).toThrow("negatives not allowed: -15");
+        })
+        it("should throw error for: '-15,-2'", function (){
+            expect(function () {calculator.add('-15,-2');}).toThrow("negatives not allowed: -15, -2");
+        })
+    });
+
+    describe("oooh", function(){
+        calculator_helper('2,1001', 2);
+        calculator_helper('2,2001', 2);
+    });
+
+    describe("char[]", function(){
+       calculator_helper('//[***]\n1***2***3', 6);
+    });
+    describe("char[][]", function(){
+        calculator_helper('//[*][%]\n1*2%3', 6);
+    });
+    describe("char[][] 2", function(){
+        calculator_helper('//[K][AC][AAA][A]\n1K2A3AAA3AC5C23', 14);
     });
 });
+
+function calculator_helper(expression, result){
+    it("should evaluate '"+expression+"' to "+result+".", function() {
+        expect(calculator.add(expression)).toBe(result);
+    });
+}
